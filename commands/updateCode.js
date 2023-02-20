e97e29e5130915770ab161c6ffa7ef26e6659186
blake-nx <blakewenloe@yahoo.com>
2023-02-19 21:08:31 -0600

    added updateCode command

---
+++ commands/updateCode.js
@@ -0,0 +1,38 @@
+const { SlashCommandBuilder } = require("discord.js");
+const User = require("../db/db-connect.js");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("updatecode")
+    .setDescription("Update your friend code")
+    .addStringOption((option) =>
+      option
+        .setName("friendcode")
+        .setDescription("Add your updated friend code!")
+        .setRequired(true)
+    ),
+  async execute(interaction) {
+    await interaction.deferReply();
+    const userId = interaction.user.id;
+    const handle = interaction.user.tag;
+    const friendCode = interaction.options.getString("friendcode");
+    try {
+      let updateCode = await User.update(
+        { friend_code: friendCode },
+        { where: { handle: handle } }
+      );
+      if (updateCode !== 0) {
+        return await interaction.editReply({
+          content: `friend code for <@${userId}> updated!`,
+          ephemeral: true,
+        });
+      }
+      await interaction.editReply({
+        content: `No friend code found for <@${userId}>`,
+        ephemeral: true,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  },
+};
