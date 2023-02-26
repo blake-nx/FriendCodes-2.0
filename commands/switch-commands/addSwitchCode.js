63c44324236148b97beb60f9d14c4429b29353ca
blake-nx <blakewenloe@yahoo.com>
2023-02-25 19:13:40 -0600

    created slash command for adding and retrieving switch friend codes

--- commands/switch-commands/addSwitchCode.js
+++ commands/switch-commands/addSwitchCode.js
@@ -1,21 +1,10 @@
 const { SlashCommandBuilder } = require("discord.js");
 const User = require("../../db/db-connect.js");
 
-function formatFriendCode(code) {
-  // Remove dashes and spaces from the code
-  const cleanedCode = code.replace(/[-\s]/g, "");
-
-  // Split the cleaned string into groups of 4 characters each
-  const groups = cleanedCode.match(/.{1,4}/g);
-
-  // Join the groups with spaces and return the formatted string e.g. 1234 5678 9012.
-  return groups.join(" ");
-}
-
 module.exports = {
   data: new SlashCommandBuilder()
-    .setName("add-pogocode")
-    .setDescription("Add your Pokemon Go friend code for easy sharing!")
+    .setName("add-switchcode")
+    .setDescription("Add your Nintendo Switch friend code for easy sharing!")
     .addStringOption((option) =>
       option
         .setName("friendcode")
@@ -33,11 +22,11 @@ module.exports = {
       if (!userExists) {
         addCode = await User.create({
           handle: handle,
-          friend_code: formatFriendCode(code),
+          switch_code: code,
         });
       } else {
         addCode = await User.update(
-          { friend_code: formatFriendCode(code) },
+          { switch_code: code },
           { where: { handle: handle } }
         );
       }
@@ -55,11 +44,11 @@ module.exports = {
           content:
             `Friend code for <@${userId}> already exists! Try ` +
             "`" +
-            "/update-pogocode" +
+            "/update-switchcode" +
             "`" +
             " to change your code or " +
             "`" +
-            "/delete-pogocode" +
+            "/delete-switchcode" +
             "`" +
             "to remove it",
           ephemeral: true,
---
+++ commands/switch-commands/getMySwitchCode.js
@@ -0,0 +1,28 @@
+const { SlashCommandBuilder } = require("discord.js");
+const User = require("../../db/db-connect.js");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("my-switchcode")
+    .setDescription("This posts your Nintendo Switch friend code!"),
+  async execute(interaction) {
+    await interaction.deferReply({ ephemeral: false });
+    const handle = interaction.user.tag;
+    const userId = interaction.user.id;
+    try {
+      const switchCode = await User.findOne({ where: { handle: handle } });
+      if (switchCode.get("switch_code") !== null) {
+        return await interaction.editReply({
+          content: `${switchCode.get("switch_code")}`,
+          ephemeral: false,
+        });
+      }
+      return await interaction.editReply({
+        content: `No friend code found for <@${userId}> <:ttar:711069119184764928>`,
+        ephemeral: true,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  },
+};
