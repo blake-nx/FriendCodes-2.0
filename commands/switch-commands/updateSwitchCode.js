5f142406de3e0505113bdde23a1cfea7944e6c5e
blake-nx <blakewenloe@yahoo.com>
2023-02-25 19:18:29 -0600

    created update switch friend code slash command

--- commands/switch-commands/updateSwitchCode.js
+++ commands/switch-commands/updateSwitchCode.js
@@ -1,21 +1,10 @@
 const { SlashCommandBuilder } = require("discord.js");
 const User = require("../../db/db-connect.js");
 
-function formatFriendCode(code) {
-  // Remove dashes and spaces from the code
-  const cleanedCode = code.replace(/[-\s]/g, "");
-
-  // Split the cleaned string into groups of 4 characters each
-  const groups = cleanedCode.match(/.{1,4}/g);
-
-  // Join the groups with spaces and return the formatted string e.g. 1234 5678 9012.
-  return groups.join(" ");
-}
-
 module.exports = {
   data: new SlashCommandBuilder()
-    .setName("update-pogocode")
-    .setDescription("Update your Pokemon Go friend code")
+    .setName("update-switchcode")
+    .setDescription("Update your Nintendo Switch friend code!")
     .addStringOption((option) =>
       option
         .setName("friendcode")
@@ -26,20 +15,20 @@ module.exports = {
     await interaction.deferReply({ ephemeral: true });
     const userId = interaction.user.id;
     const handle = interaction.user.tag;
-    const friendCode = interaction.options.getString("friendcode");
+    const switchcode = interaction.options.getString("friendcode");
     try {
       let updateCode = await User.update(
-        { friend_code: formatFriendCode(friendCode) },
+        { switch_code: switchcode },
         { where: { handle: handle } }
       );
       if (updateCode !== 0) {
         return await interaction.editReply({
-          content: `friend code updated for <@${userId}>!`,
+          content: `Nintendo Switch friend code updated for <@${userId}>!`,
           ephemeral: true,
         });
       }
       await interaction.editReply({
-        content: `Something went wrong with adding your code. Friend code doesn't exist or code length exceeds limit.`,
+        content: `Something went wrong with adding your code. Friend code doesn't exist or there was an error with its formatting.`,
         ephemeral: true,
       });
     } catch (error) {
